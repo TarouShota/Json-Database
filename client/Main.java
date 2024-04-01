05e18952af798e565bfbbf77aa2b922bdad7170c
TarouShota <adilov_02@mail.ru>
2024-04-02 01:34:39 +0200

    keep the server alive; create only 1 instance of server class

--- client/Main.java
+++ client/Main.java
@@ -10,6 +10,7 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 public class Main {
@@ -18,7 +19,7 @@ public class Main {
     @Parameter(names = {"--index", "-i"})
     Integer index;
     @Parameter(names = {"--mValue", "-m"})
-    String text;
+    String textValue;
 
     public static void main(String... argv) {
         Main main = new Main();
@@ -30,24 +31,24 @@ public class Main {
     }
 
     public void run() {
-        System.out.printf("%s %d %s", requestType, index, text);
         System.out.println("Client started!");
         String address = "127.0.0.1";
         int port = 23456;
-
+        String toSend = requestType + " " + index + " " + textValue;
+        // System.out.println(toSend);
         try {
             Socket socket = new Socket(InetAddress.getByName(address), port);
             DataInputStream input = new DataInputStream(socket.getInputStream());
             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
-            output.writeUTF("Give me a record # " + 12);
-            System.out.println("Sent: Give me a record # 12");
+
+            output.writeUTF(toSend);
+            System.out.println("Sent: " + toSend);
             System.out.println("Received: " + input.readUTF());
 
         } catch (IOException exception) {
             System.err.println("Exception riot: " + exception.getMessage());
         }
-
 
     }
 }
--- server/Main.java
+++ server/Main.java
@@ -7,88 +7,74 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         String address = "127.0.0.1";
         int port = 23456;
+        System.out.println("Server started!");
+        Server server = new Server();
+        while (true) {
+            try (ServerSocket socketServer = new ServerSocket(port, 50, InetAddress.getByName(address));) {
 
-        try {
-            ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));
-            System.out.println("Server started!");
-            Socket socket = server.accept();
-            DataInputStream input = new DataInputStream(socket.getInputStream());
-            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                Socket socket = socketServer.accept();
 
-            String utfOutput = input.readUTF();
-            System.out.println("Received: "+ utfOutput);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
-            StringBuilder number = new StringBuilder();
-            for (int i = 0; i < utfOutput.length(); i++) {
-                if (utfOutput.charAt(i) > 47 && utfOutput.charAt(i) < 58) {
-                    number.append(utfOutput.charAt(i));
+                String utfOutput = input.readUTF();
+                String[] receivedText = utfOutput.trim().split(" +");
+
+
+                String action = receivedText[0];
+                if (Objects.equals(action, "exit")) {
+                    output.writeUTF("OK");
+                    return;
                 }
+
+                int index = Integer.parseInt(receivedText[1]);
+                if (index > Server.size - 1 || index < 0) {
+                    output.writeUTF("ERROR");
+                }
+
+                StringBuilder text = new StringBuilder();
+
+                String extraSpace = "";
+                for (int i = 2; i < receivedText.length; i++) {
+                    text.append(extraSpace).append(receivedText[i]);
+                    extraSpace = " ";
+                }
+
+
+                switch (action) {
+                    case "get": {
+                        output.writeUTF(server.get(index));
+                        break;
+                    }
+                    case "set": {
+                        output.writeUTF(server.set(index, text.toString()));
+                        break;
+                    }
+                    case "delete": {
+                        output.writeUTF(server.delete(index));
+                        break;
+                    }
+                    default: {
+                        return;
+                    }
+                }
+            } catch (IOException exception) {
+                System.err.println("Caught IOException: " + exception.getMessage());
             }
-            String outputString = "A record # " + number + " was sent!";
-            output.writeUTF(outputString);
-            System.out.println("Sent: "+outputString);
 
 
-//
-        } catch (IOException exception) {
-            System.err.println("Caught IOException: " + exception.getMessage());
         }
-
-
-//        Server server = new Server();
-//        System.out.println("Type your input:");
-//        Scanner scanner = new Scanner(System.in);
-
-//        while (true) {
-//            String[] input = scanner.nextLine().trim().split(" +");
-//            String action = input[0];
-//            if(Objects.equals(action,"exit")){
-//                return;
-//            }
-//
-//            int index = Integer.parseInt(input[1]);
-//            if (index > Server.size - 1 || index < 0) {
-//                System.out.println("Wrong boundaries");
-//                return;
-//            }
-//
-//            StringBuilder text = new StringBuilder();
-//
-//            String extraSpace = "";
-//            for (int i = 2; i < input.length; i++) {
-//                text.append(extraSpace).append(input[i]);
-//                extraSpace=" ";
-//
-//            }
-
-//            switch (action) {
-//                case "get": {
-//                    System.out.println(server.get(index));
-//                    break;
-//                }
-//                case "set": {
-//                    System.out.println(server.set(index, text.toString()));
-//                    break;
-//                }
-//                case "delete": {
-//                    System.out.println(server.delete(index));
-//                    break;
-//                }
-//                default: {
-//                    return;
-//                }
-//            }
-//        }
 
 
     }

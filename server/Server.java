7c85724fd84fa14ec6eda7a52a800340e92b29f2
TarouShota <adilov_02@mail.ru>
2024-04-02 01:33:36 +0200

    increase database size to 1000

--- server/Server.java
+++ server/Server.java
@@ -3,28 +3,21 @@ package server;
 import java.util.Arrays;
 
 public class Server {
-    static int size=101;
+    static int size = 1001;
     private final String[] database = new String[size];
 
-    public Server(){
-        Arrays.fill(this.database,"");
-    }
-    public String get(int index)
-    {
-        if(this.database[index].isBlank()) return "ERROR";
+    public String get(int index) {
+        if (this.database[index] == null || this.database[index].isBlank()) return "ERROR";
         return this.database[index];
     }
 
-    private boolean isIndexOutOfBounds(int index) {
-        return index > size - 1 || index < 0;
+    public String set(int index, String text) {
+        database[index] = text;
+        return "OK";
     }
 
-    public String set(int index,String text){
-       database[index] = text;
-       return "OK";
-    }
-    public String delete(int index){
-        database[index]="";
+    public String delete(int index) {
+        database[index] = "";
         return "OK";
     }
 }
